08e681e097c25e0d1c1482df13d6ea753d2aec1d
DustinOlsen <69872283+DustinOlsen@users.noreply.github.com>
2021-02-17 11:28:57 -0600

    Initial Commit

---
+++ Shared/HWSfriendBookChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWSfriendBookChallengeApp.swift
+//  Shared
+//
+//  Created by Dustin Olsen on 2/17/21.
+//
+
+import SwiftUI
+
+@main
+struct HWSfriendBookChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
